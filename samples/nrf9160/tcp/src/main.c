1ea81fc4db38f0922eff6b8ac93f0c469e2794be
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2020-02-19 15:15:21 +0100

    Add basic TCP sample

--- samples/nrf9160/tcp/src/main.c
+++ samples/nrf9160/tcp/src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Nordic Semiconductor ASA
+ * Copyright (c) 2020 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: LicenseRef-BSD-5-Clause-Nordic
  */
@@ -7,104 +7,87 @@
 #include <zephyr.h>
 #include <net/socket.h>
 #include <stdio.h>
-#include <uart.h>
 #include <string.h>
+#include <lte_lc.h>
 
-#define TCP_HOST "towel.blinkenlights.nl"
-#define TCP_PORT 23
-#define RECV_BUF_SIZE ((80 * 24) + 1)
+#define TCP_SERVER_IPV4 	"xxx.xxx.xxx.xxx"
+#define TCP_PORT 		nnnn
+#define RECV_BUF_SIZE 		((80 * 24) + 1)
 
-char recv_buf[RECV_BUF_SIZE];
+static char recv_buf[RECV_BUF_SIZE];
 
-u32_t inet_addr(u8_t a, u8_t b, u8_t c, u8_t d)
-{
-	u32_t value = 0;
-
-	value |= (u32_t)((((u8_t)(d)) << 24) & 0xFF000000);
-	value |= (u32_t)((((u8_t)(c)) << 16) & 0x00FF0000);
-	value |= (u32_t)((((u8_t)(b)) << 8) & 0x0000FF00);
-	value |= (u32_t)((((u8_t)(a)) << 0) & 0x000000FF);
-
-	return value;
-}
-
-int blocking_recv(int fd, u8_t *buf, u32_t size, u32_t flags)
+void main(void)
 {
 	int err;
+	size_t ret;
+	int client_fd;
+	struct sockaddr_in server = { 0 };
+	struct sockaddr_in local_addr = { 0 };
+	char msg[] = "hello";
 
-	do {
-		err = recv(fd, buf, size, flags);
-	} while (err < 0 && errno == EAGAIN);
+	printk("TCP client started\n");
 
-	return err;
-}
+	printk("Setting up LTE connection\n");
 
-int blocking_send(int fd, u8_t *buf, u32_t size, u32_t flags)
-{
-	int err;
+	err = lte_lc_init_and_connect();
+	if (err) {
+		printk("LTE link could not be established, error: %d\n",
+			err);
+		return;
+	}
 
-	do {
-		err = send(fd, buf, size, flags);
-	} while (err < 0 && errno == EAGAIN);
+	printk("LTE connected\n");
 
-	return err;
-}
+	server.sin_family = AF_INET;
+	server.sin_port = htons(TCP_PORT);
 
-int blocking_connect(int fd, struct sockaddr *local_addr, socklen_t len)
-{
-	int err;
-
-	do {
-		err = connect(fd, local_addr, len);
-	} while (err < 0 && errno == EAGAIN);
-
-	return err;
-}
-
-void app_tcp_socket_start(void)
-{
-	struct addrinfo *res;
-
-	printk("TCP example");
-
-	int err = getaddrinfo(TCP_HOST, NULL, NULL, &res);
-
-	printk("getaddrinfo err: %d\n\r", err);
-	((struct sockaddr_in *)res->ai_addr)->sin_port = htons(TCP_PORT);
-	struct sockaddr_in local_addr;
+	err = inet_pton(AF_INET, TCP_SERVER_IPV4, &server.sin_addr);
+	if (err != 1) {
+		printk("inet_pton failed, errno: %d\n", errno);
+		return;
+	}
 
 	local_addr.sin_family = AF_INET;
 	local_addr.sin_port = htons(0);
 	local_addr.sin_addr.s_addr = 0;
 
-	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_fd <= 0) {
+		printk("socket() failed, errno: %d\n", errno);
+		return;
+	}
 
 	printk("client_fd: %d\n\r", client_fd);
+
 	err = bind(client_fd, (struct sockaddr *)&local_addr,
 		   sizeof(local_addr));
-	printk("bind err: %d\n\r", err);
-	err = blocking_connect(client_fd, (struct sockaddr *)res->ai_addr,
-			       sizeof(struct sockaddr_in));
-	printk("connect err: %d\n\r", errno);
+	if (err) {
+		printk("bind failed, errno: %d\n\r", errno);
+		goto exit;
+	}
 
-	size_t ret;
+	err = connect(client_fd, (struct sockaddr *)&server,
+		      sizeof(struct sockaddr_in));
+	if (err) {
+		printk("connect failed, errno: %d\n\r", errno);
+		goto exit;
+	}
 
-	do {
-		ret = blocking_recv(client_fd, recv_buf, sizeof(recv_buf) - 1,
-				    0);
-		if (ret > 0 && ret < RECV_BUF_SIZE) {
-			/* Ensure that string is zero terminated */
-			recv_buf[ret] = 0;
-			printk("%s", recv_buf);
-		}
-	} while (ret > 0);
 
-	printk("\n\rfin");
-	freeaddrinfo(res);
-	err = close(client_fd);
-}
+	err = send(client_fd, msg, strlen(msg), 0);
+	if (err < 0) {
+		printk("Failed to send data, errno: %d\n", errno);
+		goto exit;
+	}
 
-int main(void)
-{
-	app_tcp_socket_start();
+	printk("Data sent successfully, waiting for response...\n");
+
+	ret = recv(client_fd, recv_buf, sizeof(recv_buf) - 1, 0);
+	if (ret > 0 && ret < RECV_BUF_SIZE) {
+		recv_buf[ret] = 0;
+		printk("Response: %s\n", recv_buf);
+	}
+
+exit:
+	(void)close(client_fd);
 }
