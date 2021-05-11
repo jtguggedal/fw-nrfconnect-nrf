09bc2b65945023f33c47b2d5f49f721aaab5f4cc
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2021-05-11 17:42:48 +0200

    [WIP] lib: memfault: Add configuration options for HW and FWo

--- lib/memfault/memfault_integration.c
+++ lib/memfault/memfault_integration.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#ifdef CONFIG_MEMFAULT_DEVICE_ID_USE_IMEI
+#ifdef CONFIG_MEMFAULT_DEVICE_SERIAL_USE_IMEI
 #include <modem/at_cmd.h>
 #include <init.h>
 #endif
@@ -22,13 +22,31 @@
 
 #define IMEI_LEN 15
 
-static char s_fw_version[20] = "v0.0.1+";
-static char s_device_serial[MAX(sizeof(CONFIG_MEMFAULT_DEVICE_ID), IMEI_LEN + 1)] =
-	CONFIG_MEMFAULT_DEVICE_ID;
+/* API key check */
+BUILD_ASSERT(sizeof(CONFIG_MEMFAULT_API_KEY) > 1, "API key must be configured");
 
-BUILD_ASSERT(sizeof(CONFIG_MEMFAULT_API_KEY) > 2, "API key must be configured");
-BUILD_ASSERT((sizeof(CONFIG_MEMFAULT_DEVICE_ID) > 2) || CONFIG_MEMFAULT_DEVICE_ID_USE_IMEI,
+/* Device ID check */
+BUILD_ASSERT((sizeof(CONFIG_MEMFAULT_DEVICE_ID) > 1) || CONFIG_MEMFAULT_DEVICE_SERIAL_USE_IMEI,
 	"Device ID must be set or condifured to use IMEI");
+
+/* Firmware type check */
+BUILD_ASSERT(sizeof(CONFIG_MEMFAULT_FW_TYPE) > 1, "Firmware type must be configured");
+
+/* Firmware version checks */
+#if defined(MEMFAULT_FW_VERSION_STATIC)
+BUILD_ASSERT(sizeof(CONFIG_MEMFAULT_FW_VERSION_STATIC) > 1, "Firmware version must be configured");
+#endif
+
+static char fw_version[sizeof(CONFIG_MEMFAULT_FW_VERSION_PREFIX) + 8] =
+	CONFIG_MEMFAULT_FW_VERSION_PREFIX;
+
+#if defined(CONFIG_MEMFAULT_DEVICE_SERIAL_USE_IMEI)
+static char device_serial[MAX(sizeof(CONFIG_MEMFAULT_DEVICE_ID), IMEI_LEN + 1)] =
+	CONFIG_MEMFAULT_DEVICE_ID;
+#else
+static char device_serial[] = CONFIG_MEMFAULT_DEVICE_ID;
+#endif
+
 
 void memfault_platform_get_device_info(sMemfaultDeviceInfo *info)
 {
@@ -36,24 +54,24 @@ void memfault_platform_get_device_info(sMemfaultDeviceInfo *info)
 
 	if (!s_init)
 	{
-		const size_t version_len = strlen(s_fw_version);
+		const size_t version_len = strlen(fw_version);
 		// We will use 6 characters of the build id to make our versions unique and
 		// identifiable between releases
 		const size_t build_id_chars = 6 + 1 /* '\0' */;
 
 		const size_t build_id_num_chars =
-		    MIN(build_id_chars, sizeof(s_fw_version) - version_len - 1);
+		    MIN(build_id_chars, sizeof(fw_version) - version_len - 1);
 
-		memfault_build_id_get_string(&s_fw_version[version_len], build_id_num_chars);
+		memfault_build_id_get_string(&fw_version[version_len], build_id_num_chars);
 		s_init = true;
 	}
 
 	// platform specific version information
 	*info = (sMemfaultDeviceInfo){
-		.device_serial = s_device_serial,
-		.software_type = "nrf91ns-fw",
-		.software_version = s_fw_version,
-		.hardware_version = "proto",
+		.device_serial = device_serial,
+		.software_type = CONFIG_MEMFAULT_FW_TYPE,
+		.software_version = fw_version,
+		.hardware_version = CONFIG_MEMFAULT_HW_VERSION,
 	};
 }
 
@@ -61,7 +79,7 @@ sMfltHttpClientConfig g_mflt_http_client_config = {
     .api_key = CONFIG_MEMFAULT_API_KEY,
 };
 
-#ifdef CONFIG_MEMFAULT_DEVICE_ID_USE_IMEI
+#ifdef CONFIG_MEMFAULT_DEVICE_SERIAL_USE_IMEI
 
 static int query_modem(const char *cmd, char *buf, size_t buf_len)
 {
@@ -84,16 +102,16 @@ static int prv_init_device_info(const struct device *unused)
 	char imei_buf[IMEI_LEN + 2 /* for \r\n */ + 1 /* \0 */];
 	if (query_modem("AT+CGSN", imei_buf, sizeof(imei_buf)) != 0)
 	{
-		strcat(s_device_serial, "Unknown");
+		strcat(device_serial, "Unknown");
 		return 0;
 	}
 
 	imei_buf[IMEI_LEN] = '\0';
-	strcat(s_device_serial, imei_buf);
+	strcat(device_serial, imei_buf);
 
 	return 0;
 }
 
 SYS_INIT(prv_init_device_info, APPLICATION, CONFIG_MEMFAULT_INIT_PRIORITY);
 
-#endif /* CONFIG_MEMFAULT_DEVICE_ID_USE_IMEI */
+#endif /* CONFIG_MEMFAULT_DEVICE_SERIAL_USE_IMEI */
