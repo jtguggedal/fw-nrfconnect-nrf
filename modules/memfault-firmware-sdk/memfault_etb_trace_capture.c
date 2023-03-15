8097c0dd4b6a6e1b241a078ac0c81ce6bf8fc075
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2023-03-15 11:03:37 +0100

    modules: memfault: Add ETB trace dump support to Memfault library
    Adds support for including the content of Embedded Trace Buffer
    in a coredump.
    
    Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>

---
+++ modules/memfault-firmware-sdk/memfault_etb_trace_capture.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/types.h>
+#include <debug/etb_trace.h>
+#include <memfault/panics/platform/coredump.h>
+
+#define ETB_BUFFER_VALID_MAGIC 	0xDEADBEEF
+
+static volatile uint32_t etb_buf[ETB_BUFFER_SIZE / 4];
+static volatile uint32_t etb_buf_valid;
+
+void memfault_platform_fault_handler(const sMfltRegState *regs,
+                                     eMemfaultRebootReason reason)
+{
+	ARG_UNUSED(regs);
+	ARG_UNUSED(reason);
+
+	etb_trace_stop();
+	etb_data_get(etb_buf, ARRAY_SIZE(etb_buf));
+
+	etb_buf_valid = ETB_BUFFER_VALID_MAGIC;
+}
