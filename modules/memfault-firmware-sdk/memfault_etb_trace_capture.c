b062a64209dbf70002a0ca03371307dcff739701
Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
2023-04-17 15:44:01 +0200

    wip: Reduce power consumption

--- modules/memfault-firmware-sdk/memfault_etb_trace_capture.c
+++ modules/memfault-firmware-sdk/memfault_etb_trace_capture.c
@@ -7,8 +7,11 @@
 #include <zephyr/types.h>
 #include <debug/etb_trace.h>
 #include <memfault/panics/platform/coredump.h>
+#include <zephyr/wait_q.h>
+#include <zephyr/pm/pm.h>
 
-#define ETB_BUFFER_VALID_MAGIC 	0xDEADBEEF
+#define MIN_IDLE_TIME_TRACE_SHUTDOWN_MS		1000
+#define ETB_BUFFER_VALID_MAGIC 			0xDEADBEEF
 
 static volatile uint32_t etb_buf[ETB_BUFFER_SIZE / 4];
 static volatile uint32_t etb_buf_valid;
@@ -24,3 +27,38 @@ void memfault_platform_fault_handler(const sMfltRegState *regs,
 
 	etb_buf_valid = ETB_BUFFER_VALID_MAGIC;
 }
+
+
+static bool trace_action;
+
+void sys_clock_idle_exit(void)
+{
+
+	if (trace_action) {
+		etb_trace_start();
+	}
+}
+
+static void idle_override()
+{
+	while (1) {
+		(void) arch_irq_lock();
+
+		_kernel.idle = z_get_next_timeout_expiry();
+
+		trace_action = _kernel.idle > k_ms_to_ticks_ceil32(MIN_IDLE_TIME_TRACE_SHUTDOWN_MS);
+
+		if (trace_action) {
+			etb_trace_stop();
+			NRF_TAD_S->TASKS_CLOCKSTOP = TAD_TASKS_CLOCKSTOP_TASKS_CLOCKSTOP_Msk;
+		}
+
+		k_cpu_idle();
+
+		if (trace_action) {
+			etb_trace_start();
+		}
+	}
+}
+
+K_THREAD_DEFINE(idle_override_thread, 1024, hacky_idle, NULL, NULL, NULL, K_IDLE_PRIO - 1, 0, 0);
